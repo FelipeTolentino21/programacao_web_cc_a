3ae3e28509c2269b33417d3fd77f35bfdf3a2109
FelipeTolentino21 <tolentinofelipe21@gmail.com>
2024-08-16 08:35:01 -0300

    Adicionado no JavaScript um regex e uma expressão que valida o email inserido.

--- aulaScript.js
+++ aulaScript.js
@@ -2,13 +2,32 @@ let botaoSubmeter = document.getElementById("botao-submeter");
 
 botaoSubmeter.addEventListener("click", submeter);
 
-function submeter(){
+function submeter() {
     let elementoNome = document.getElementById("nome").value;
     let elementoIdade = document.getElementById("idade").value;
     let elementoEmail = document.getElementById("email").value;
+
     console.log(elementoNome);
     console.log(elementoIdade);
     console.log(elementoEmail);
+    
+    if (validacao()) {
+        console.log("Formulário válido!");
+    } else {
+        console.log("Formulário inválido!");
+    }
 }
 
-// Colocar script que valida o email
+function validacao() {
+    let validacaoEmail = document.getElementById("email").value;
+
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (regex.test(validacaoEmail)) {
+        alert("Email VÁLIDO!");
+        return true;
+    } else {
+        alert("Email INVÁLIDO!");
+        return false;
+    }
+}
