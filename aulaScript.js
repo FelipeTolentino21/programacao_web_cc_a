7b364cbc01cf85e414553350c605bbf53400825c
FelipeTolentino21 <tolentinofelipe21@gmail.com>
2024-08-19 10:56:44 -0300

    Atualização final no JS do site da aula

--- aulaScript.js
+++ aulaScript.js
@@ -6,43 +6,16 @@ function submeter() {
     let elementoNome = document.getElementById("nome").value;
     let elementoIdade = document.getElementById("idade").value;
     let elementoEmail = document.getElementById("email").value;
-    /*
-    console.log(elementoNome);
-    console.log(elementoIdade);
-    console.log(elementoEmail);
-    */
+    
     if(validaEmail(elementoEmail) == true){
+        console.log(elementoNome);
+        console.log(elementoIdade);
         console.log(elementoEmail);
     } else {
         alert("Inválido");
     }
-
-    /*      MEU REGEX ANTIGO
-    if (validacao()) {
-        console.log("Formulário válido!");
-    } else {
-        console.log("Formulário inválido!");
-    }
-    */
-
 }
 
 function validaEmail(email){
-    return /^[a-zA-Z0-9]+(?:[\.\-\_][a-zA-Z0-9]+)*@[a-zA-Z]+\.[a-zA-Z]+(?:[\.\-][a-zA-Z]+)*$/.test(email);
+    return /^[a-zA-Z0-9]+(?:[\.\-\_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:[\.][a-zA-Z]+)*$/.test(email);
 }
-
-/*          MEU REGEX ANTIGO
-function validacao() {
-    let validacaoEmail = document.getElementById("email").value;
-
-    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (regex.test(validacaoEmail)) {
-        alert("Email VÁLIDO!");
-        return true;
-    } else {
-        alert("Email INVÁLIDO!");
-        return false;
-    }
-}
-*/
