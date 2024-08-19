3246e24a72287980836faddb7e36767489f20726
FelipeTolentino21 <tolentinofelipe21@gmail.com>
2024-08-19 10:13:40 -0300

    Alterações no JS e na validação do email

--- aulaScript.js
+++ aulaScript.js
@@ -6,18 +6,32 @@ function submeter() {
     let elementoNome = document.getElementById("nome").value;
     let elementoIdade = document.getElementById("idade").value;
     let elementoEmail = document.getElementById("email").value;
-
+    /*
     console.log(elementoNome);
     console.log(elementoIdade);
     console.log(elementoEmail);
-    
+    */
+    if(validaEmail(elementoEmail) == true){
+        console.log(elementoEmail);
+    } else {
+        alert("Inválido");
+    }
+
+    /*      MEU REGEX ANTIGO
     if (validacao()) {
         console.log("Formulário válido!");
     } else {
         console.log("Formulário inválido!");
     }
+    */
+
 }
 
+function validaEmail(email){
+    return /^[a-zA-Z0-9]+(?:[\.\-\_][a-zA-Z0-9]+)*@[a-zA-Z]+\.[a-zA-Z]+(?:[\.\-][a-zA-Z]+)*$/.test(email);
+}
+
+/*          MEU REGEX ANTIGO
 function validacao() {
     let validacaoEmail = document.getElementById("email").value;
 
@@ -31,3 +45,4 @@ function validacao() {
         return false;
     }
 }
+*/
